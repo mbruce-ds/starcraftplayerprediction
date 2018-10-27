68a319ce99f9ab27a7a556ce74134d6f15241de8
dylankbruce <dylankbruce@gmail.com>
2018-10-26 22:18:09 -0400

    0.87941, changed model from SVC to random forest, added epm and another spam measurement

--- analysis.py
+++ analysis.py
@@ -5,27 +5,35 @@
 
 
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
 
 train_features_path = 'features_train.csv'
 test_features_path = 'features_test.csv'
 train_dataset = pd.read_csv(train_features_path)
 test_dataset = pd.read_csv(test_features_path)
-y_train = train_dataset.battleneturl
-X_train = pd.get_dummies(train_dataset, columns = ['race']).drop(['battleneturl', 'playername'], axis=1)
-print(X.columns)
+train_dataset = pd.get_dummies(train_dataset, columns = ['race'])
+test_dataset = pd.get_dummies(test_dataset, columns = ['race'])
+y = train_dataset.battleneturl
+X = train_dataset.drop(['battleneturl', 'playername'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 1)
-svc = svm.SVC(kernel = 'linear')
-svc.fit(X_train, y_train)
-print(svc.score(X_test, y_test))
+rfc = RandomForestClassifier(n_estimators = 100)
+rfc.fit(X_train, y_train)
+print(rfc.score(X_test, y_test))
 
-X_test = pd.get_dummies(test_dataset, columns = ['race'])
 
-full_svc = svm.SVC(kernel = 'linear')
-full_svc.fit(X_train, y_train)
-predictions = full_svc.predict(X_test)
+fig, ax = plt.subplots(figsize=(16, 12))
+sns.heatmap(train_dataset.corr(), annot=True, fmt='.2f', ax = ax, linewidths=0.5, cmap='viridis')
+fig.savefig('correlations.png')
+plt.close(fig)
+
+full_rfc = RandomForestClassifier(n_estimators = 100)
+full_rfc.fit(X, y)
+predictions = full_rfc.predict(test_dataset)
 prediction_frame = pd.DataFrame({'RowId' : range(1, 341), 'prediction' : predictions})
 write_file = 'submission.csv'
 prediction_frame.to_csv(write_file, index = False)
--- feature_engineering.py
+++ feature_engineering.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import statistics
-import time
+
 from collections import Counter
 
 # we'll only be using the more detailed dataset, ignore the smaller one
@@ -12,10 +12,14 @@ test_path_long  = '../input/test_long.csv'
 train = pd.read_csv(train_path_long, low_memory = False)
 train = train.drop(['Unnamed: 0'], axis = 1)
 train = train.rename(columns = {'0': 'battleneturl', '1' : 'race'})
+train.name = 'train'
 
 test = pd.read_csv(test_path_long, low_memory = False)
 test = test.drop(['Unnamed: 0'], axis = 1)
 test = test.rename(columns = {'0' : 'race'})
+test.name = 'test'
+
+all_data = [train, test]
 
 # creates variable with the player name using the url, just for ease of reading
 
@@ -60,104 +64,87 @@ player_races_played = Counter(name_split_race).most_common()
 # extract the features and put them into a new dataframe that we will predict
 # with. this way we can also just save the features for later use and we won't
 # have to always reprocess the dataset every time we want to test something
-X_columns = ['battleneturl', 'playername', 'race', 'gamelength', 'apm', '1_usage', '2_usage', '3_usage', '4_usage', '5_usage', '6_usage', '7_usage', '8_usage', '9_usage', '0_usage', 'chattiness', 'worker_box_count']
+X_columns = ['battleneturl', 'playername', 'race', 'gamelength', 'apm', 'epm', '1_usage', '2_usage', '3_usage', '4_usage', '5_usage', '6_usage', '7_usage', '8_usage', '9_usage', '0_usage', 'chattiness', 'chat_with_comma', 'worker_box_spam']
 X_train = pd.DataFrame(index = train.index, columns = X_columns)
 
-X_test_columns = ['race', 'gamelength', 'apm', '1_usage', '2_usage', '3_usage', '4_usage', '5_usage', '6_usage', '7_usage', '8_usage', '9_usage', '0_usage', 'chattiness', 'worker_box_count']
+X_test_columns = ['race', 'gamelength', 'apm', 'epm', '1_usage', '2_usage', '3_usage', '4_usage', '5_usage', '6_usage', '7_usage', '8_usage', '9_usage', '0_usage', 'chattiness', 'chat_with_comma', 'worker_box_spam']
 X_test = pd.DataFrame(index = test.index, columns = X_test_columns)
 
 workers = ['SCV', 'Probe', 'Drone']
 
-for game in train.index:
-    start = time.time()
-    chat_count = 0
-    time_frame = 0
-    total_actions = 0
-    worker_box_count = 0
-    selected = []
-    camera_x = []
-    camera_y = []
-    control_group_count = {'1' : 0, '2' : 0, '3' : 0, '4' : 0, '5' : 0, '6' : 0, '7' : 0, '8' : 0, '9' : 0, '0' : 0}
-    control_group = {'1' : [], '2' : [], '3' : [], '4' : [], '5' : [], '6' : [], '7' : [], '8' : [], '9' : [], '0' : []}
-    # dicts are way faster to iterate over than using iloc on a dataframe
-    actions = train.iloc[game].to_dict()
-    url = actions['battleneturl']
-    name = actions['playername']
-    race = actions['race']
-    for i in range(2,26825):
-        i_string = str(i)
-        if(type(actions[i_string]) is not float):
-            action_info = actions[i_string].split(':')
-            # failsafe check in case if a chatlog messed up the string splitting...
-            # which happens... thanks Golden for typing "gl, hf"
-            if(len(action_info) > 1):
-                time_frame = int(action_info[0])
-                if(action_info[1] == 'ChatEvent'):
-                    chat_count += 1
-                else:
+for dataframe in all_data:
+    if dataframe.name == 'train':
+        i_start = 2
+        i_finish = 26825
+    else:
+        i_start = 1
+        i_finish = 14626
+    for game in dataframe.index:
+        chat_count = 0
+        time_frame = 0
+        total_actions = 0
+        worker_box_spam = 0
+        control_group_spam = 0
+        comma_usage = 0
+        last_action = None
+        selected = []
+        camera_x = []
+        camera_y = []
+        control_group_count = {'1' : 0, '2' : 0, '3' : 0, '4' : 0, '5' : 0, '6' : 0, '7' : 0, '8' : 0, '9' : 0, '0' : 0}
+        control_group = {'1' : [], '2' : [], '3' : [], '4' : [], '5' : [], '6' : [], '7' : [], '8' : [], '9' : [], '0' : []}
+        # dicts are way faster to iterate over than using iloc on a dataframe
+        actions = dataframe.iloc[game].to_dict()
+        if dataframe.name == 'train':
+            url = actions['battleneturl']
+            name = actions['playername']
+        race = actions['race']
+        for i in range(i_start, i_finish):
+            i_string = str(i)
+            if(type(actions[i_string]) is not float):
+                action_info = actions[i_string].split(':')
+                # failsafe check in case if a chatlog messed up the string splitting...
+                # which happens... thanks Golden for typing "gl, hf"
+                if(len(action_info) > 1):
+                    time_frame = int(action_info[0])
+                    if(action_info[1] == 'SelectionEvent'):
+                        selected = action_info[2].split(';')
+                        if(len(selected) > 1):
+                            if(all([x in workers for x in selected])):
+                                worker_box_spam += 1
+                        last_action = 'SelectionEvent'
                     if(action_info[1] == 'ControlGroupEvent'):
                         control_group_count[action_info[2]] += 1
                         if(action_info[3] != 2):
                             control_group[action_info[2]] = selected
                         if(action_info[3] == 2):
                             selected = control_group[action_info[2]]
-                    if(action_info[1] == 'SelectionEvent'):
-                        selected = action_info[2].split(';')
-                        if(len(selected) > 1):
-                            if(all([x in workers for x in selected])):
-                                worker_box_count += 1
+                        if(last_action == 'ControlGroupEvent'):
+                            control_group_spam += 1
+                        last_action = 'ControlGroupEvent'
+                    if(action_info[1] == 'CameraEvent'):
+                        last_action = 'CameraEvent'
+                    if(action_info[1] == 'ChatEvent'):
+                        chat_count += 1
+                    if(action_info[1] == 'BasicCommandEvent'):
+                        last_action = 'BasicCommandEvent'
+                    if(action_info[1] == 'TargetPointCommandEvent'):
+                        last_action = 'TargetPointCommandEvent'
+                    if(action_info[1] == 'TargetUnitCommandEvent'):
+                        last_action = 'TargetUnitCommandEvent'
                     total_actions += 1
-        else:
-            break
-    game_length = (time_frame + 1)/ (16*60)
-    apm = total_actions / game_length
-    control_group_importance = {k : v/game_length for k, v in control_group_count.items()}
-    X_train.iloc[game] = [url, name, race, game_length, apm, control_group_importance['1'], control_group_importance['2'], control_group_importance['3'], control_group_importance['4'], control_group_importance['5'], control_group_importance['6'], control_group_importance['7'], control_group_importance['8'], control_group_importance['9'], control_group_importance['0'], chat_count, worker_box_count/min(game_length, 7)]
-    end = time.time()
-    # print(X.iloc[game], '\n\n\n', 'Processed in: {:.4f} seconds\n'.format(end - start), '-'*50, '\n')
-
-for game in test.index:
-    chat_count = 0
-    time_frame = 0
-    total_actions = 0
-    worker_box_count = 0
-    selected = []
-    camera_x = []
-    camera_y = []
-    control_group_count = {'1' : 0, '2' : 0, '3' : 0, '4' : 0, '5' : 0, '6' : 0, '7' : 0, '8' : 0, '9' : 0, '0' : 0}
-    control_group = {'1' : [], '2' : [], '3' : [], '4' : [], '5' : [], '6' : [], '7' : [], '8' : [], '9' : [], '0' : []}
-    # dicts are way faster to iterate over than using iloc on a dataframe
-    actions = test.iloc[game].to_dict()
-    race = actions['race']
-    for i in range(1,14626):
-        i_string = str(i)
-        if(type(actions[i_string]) is not float):
-            action_info = actions[i_string].split(':')
-            # failsafe check in case if a chatlog messed up the string splitting...
-            # which happens... thanks Golden for typing "gl, hf"
-            if(len(action_info) > 1):
-                time_frame = int(action_info[0])
-                if(action_info[1] == 'ChatEvent'):
-                    chat_count += 1
                 else:
-                    if(action_info[1] == 'ControlGroupEvent'):
-                        control_group_count[action_info[2]] += 1
-                        if(action_info[3] != 2):
-                            control_group[action_info[2]] = selected
-                        if(action_info[3] == 2):
-                            selected = control_group[action_info[2]]
-                    if(action_info[1] == 'SelectionEvent'):
-                        selected = action_info[2].split(';')
-                        if(len(selected) > 1):
-                            if(all([x in workers for x in selected])):
-                                worker_box_count += 1
-                    total_actions += 1
+                    comma_usage += 1
+            else:
+                break
+        game_length = (time_frame + 1)/ (16*60)
+        spam = worker_box_spam + control_group_spam
+        apm = total_actions / game_length
+        epm = (total_actions - spam) / game_length
+        control_group_importance = {k : v/game_length for k, v in control_group_count.items()}
+        if dataframe.name == 'train':
+            X_train.iloc[game] = [url, name, race, game_length, apm, epm, control_group_importance['1'], control_group_importance['2'], control_group_importance['3'], control_group_importance['4'], control_group_importance['5'], control_group_importance['6'], control_group_importance['7'], control_group_importance['8'], control_group_importance['9'], control_group_importance['0'], chat_count, comma_usage, worker_box_spam/min(game_length, 7)]
         else:
-            break
-    game_length = (time_frame + 1)/ (16*60)
-    apm = total_actions / game_length
-    control_group_importance = {k : v/game_length for k, v in control_group_count.items()}
-    X_test.iloc[game] = [race, game_length, apm, control_group_importance['1'], control_group_importance['2'], control_group_importance['3'], control_group_importance['4'], control_group_importance['5'], control_group_importance['6'], control_group_importance['7'], control_group_importance['8'], control_group_importance['9'], control_group_importance['0'], chat_count, worker_box_count/min(game_length, 7)]
+            X_test.iloc[game] = [race, game_length, apm, epm, control_group_importance['1'], control_group_importance['2'], control_group_importance['3'], control_group_importance['4'], control_group_importance['5'], control_group_importance['6'], control_group_importance['7'], control_group_importance['8'], control_group_importance['9'], control_group_importance['0'], chat_count, comma_usage, worker_box_spam/min(game_length, 7)]
 
 train_features_path = 'features_train.csv'
 test_features_path = 'features_test.csv'
